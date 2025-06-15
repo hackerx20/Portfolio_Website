6f8d90c8a81ddb9662303dcfb5658a1c0defaa9b
hackerx20 <shivansh_t@mt.iitr.ac.in>
2025-06-15 12:29:35 +0530

    Updating Portfolio website

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -22,7 +22,7 @@ const ProjectCard = ({ title, description, image, tags, link, index }) => {
                 <img
                     src={image}
                     alt={title}
-                    className="w-full h-100 object-cover"
+                    className="w-full h-80 object-cover"
                 />
                 <div className="p-6 flex flex-col flex-grow">
                     <h3 className="text-xl font-bold mb-2 ">{title}</h3>
